d152d17a5be657883af6e2890401b19c356e7614
Cliffano Subagio <cliffano@gmail.com>
2010-11-21 00:17:05 +1100

    Fixed parameter encoding/decoding.

--- public/scripts/feedtouch.js
+++ public/scripts/feedtouch.js
@@ -1,7 +1,7 @@
 var FeedTouch = function () {
 };
 FeedTouch.prototype.loadFeed = function (feedUrl, maxDisplay, numElems) {
-	var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&num=' + maxDisplay + '&q=' + feedUrl,
+	var url = 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&num=' + maxDisplay + '&q=' + encodeURIComponent(feedUrl),
 	    feed, entry, i, ln;
 	for (i = 0; i < numElems; i++) {
 	    $('li#' + i).hide();
@@ -11,14 +11,14 @@ FeedTouch.prototype.loadFeed = function (feedUrl, maxDisplay, numElems) {
 	$.getJSON(url, function (data) {
 		if (data.responseStatus === 200) {
 			feed = data.responseData.feed;
-			document.title = feed.title;
+			document.title = feed.title + ' - FeedTouch';
 		    $('li#indicator').hide();
 		    $('h1#title').text(feed.title);
 		    ln = (feed.entries.length > maxDisplay) ? maxDisplay : feed.entries.length;
 		    for (i = 0; i < ln; i++) {
 	 		    entry = feed.entries[i];
 			    $('li#' + i + ' a').text(entry.title);
-			    $('li#' + i + ' a').attr('href', '/article?title=' + entry.title + '&url=' + entry.link);
+			    $('li#' + i + ' a').attr('href', '/article?url=' + entry.link + '&title=' + encodeURIComponent(entry.title));
 			    $('li#' + i).show();
 		    }
 		} else {
@@ -28,7 +28,7 @@ FeedTouch.prototype.loadFeed = function (feedUrl, maxDisplay, numElems) {
 	});
 };
 FeedTouch.prototype.loadArticle = function (articleUrl, articleTitle) {
-    var url = 'http://viewtext.org/api/text?url=' + articleUrl + '&callback=?';
+    var url = 'http://viewtext.org/api/text?url=' + encodeURIComponent(articleUrl) + '&callback=?';
     $.getJSON(url, function (data) {
         $('div#content').html('<p><strong>' + articleTitle + '</strong><br/><a href="' + articleUrl + '">' + articleUrl + '</a></p>' + data.content);
     });
