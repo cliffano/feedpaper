fe1363d5a0ee3c0adedb21120b3ee1c4b786b3aa
Cliffano Subagio <cliffano@gmail.com>
2011-02-26 15:59:47 +1100

    More test scenarios.

--- test/web/main.rb
+++ test/web/main.rb
@@ -21,5 +21,44 @@ class MainTest < Test::Unit::TestCase
         @b.goto(@url + '/')
         assert(@b.contains_text('Why FeedTouch?'))
         
+        # check feed discovery of a site having a single feed
+        @b.goto(@url + '/wired.com')
+        assert(@b.contains_text('App by Studio Cliffano'))
+        assert_equal('FeedTouch', @b.title)
+        # check first article
+        articleLink = @b.li(:id, '0').link(:class, 'ui-link-inherit')
+        articleTitle = articleLink.title
+        articleLink.click
+        assert(@b.contains_text(articleTitle))
+
+        # check feed discovery of a site having multiple feeds
+        @b.goto(@url + '/ma.tt')
+        assert_equal('FeedTouch', @b.title)
+        # check first feed
+        feedLink = @b.li(:id, '0').link(:class, 'ui-link-inherit')
+        feedTitle = feedLink.title
+        feedLink.click
+        assert(@b.contains_text(feedTitle))
+        # check first article
+        articleLink = @b.li(:id, '0').link(:class, 'ui-link-inherit')
+        articleTitle = articleLink.title
+        articleLink.click
+        assert(@b.contains_text(articleTitle))
+
+        # check direct feed url
+        @b.goto(@url + '/news.ycombinator.com/rss')
+        # check first article
+        articleLink = @b.li(:id, '0').link(:class, 'ui-link-inherit')
+        articleTitle = articleLink.title
+        articleLink.click
+        assert(@b.contains_text(articleTitle))
+        
+        # check feed via url query string
+        @b.goto(@url + '/?url=http://news.ycombinator.com/rss')
+        # check first article
+        articleLink = @b.li(:id, '0').link(:class, 'ui-link-inherit')
+        articleTitle = articleLink.title
+        articleLink.click
+        assert(@b.contains_text(articleTitle))
     end
 end
