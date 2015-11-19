4eb67064f85c4a7b4a155919a6ed7586aa5569ba
Cliffano Subagio <cliffano@gmail.com>
2015-11-20 08:11:53 +1100

    Use FEEDPAPER_ENV for consistency.

--- feedpaper-web/params.js
+++ feedpaper-web/params.js
@@ -1,11 +1,23 @@
 var fs   = require('fs');
 var p    = require('path');
 var slug = require('slug');
+var url  = require('url');
+var util = require('util');
 
 slug.defaults.mode ='rfc3986';
 
-var env = process.env['FEEDPAPER_ENV'] || 'stg';
+var env = process.env['FEEDPAPER_ENV'];
 var feedsCategories = JSON.parse(fs.readFileSync(p.join('..', 'conf', env, 'feeds.json')));
+var conf = JSON.parse(fs.readFileSync(p.join('..', 'conf', env, 'feedpaper.json')));
+
+// in lieu of AE86 pre-hook
+var apiBase = url.format({
+  protocol: conf.api.protocol,
+  hostname: conf.api.host,
+  pathname: util.format('v%d/%s', conf.api.version, conf.api.path)
+});
+var globalJs = fs.readFileSync(p.join('static', 'scripts', 'global.js'));
+globalJs = globalJs.replace(/var apiBase = '.*';/, util.format('var apiBase = \'%s\';', apiBase));
 
 exports.params = {
   slug: function(title, cb) {
