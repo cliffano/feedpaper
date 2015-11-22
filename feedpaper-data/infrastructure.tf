d13218e98814d7ac3bc65fc862c0119123f44d68
Cliffano Subagio <cliffano@gmail.com>
2015-11-22 11:19:23 +1100

    Use one dynamodb table to reduce cost.

--- feedpaper-data/infrastructure.tf
+++ feedpaper-data/infrastructure.tf
@@ -1,6 +1,4 @@
-variable "db_data_conf" {}
-variable "db_data_feeds" {}
-variable "db_data_articles" {}
+variable "db_data" {}
 
 provider "aws" {
     region = "ap-southeast-2"
@@ -11,50 +9,14 @@ provider "aws" {
     region = "us-west-2"
 }
 
-resource "aws_dynamodb_table" "data_conf" {
+resource "aws_dynamodb_table" "data" {
     provider = "aws.us"
-    name = "${var.db_data_conf}"
-    read_capacity = 5
-    write_capacity = 5
+    name = "${var.db_data}"
+    read_capacity = 1
+    write_capacity = 1
     hash_key = "id"
     attribute {
       name = "id"
-      type = "S"
-    }
-    attribute {
-      name = "url"
-      type = "S"
-    }
-}
-
-resource "aws_dynamodb_table" "data_feeds" {
-    provider = "aws.us"
-    name = "${var.db_data_feeds}"
-    read_capacity = 5
-    write_capacity = 5
-    hash_key = "url"
-    attribute {
-      name = "url"
-      type = "S"
-    }
-    attribute {
-      name = "articles"
-      type = "S"
-    }
-}
-
-resource "aws_dynamodb_table" "data_articles" {
-    provider = "aws.us"
-    name = "${var.db_data_articles}"
-    read_capacity = 5
-    write_capacity = 5
-    hash_key = "url"
-    attribute {
-      name = "url"
-      type = "S"
-    }
-    attribute {
-      name = "content"
       type = "S"
     }
 }
