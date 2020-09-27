c422b970683dcbeba61544f5c3eb62f6eb16d583
Cliffano Subagio <cliffano@gmail.com>
2020-09-27 20:08:26 +1000

    Turn params.js to esm following upgrade to ae86 2.0.0 .

--- feedpaper-web/params.js
+++ feedpaper-web/params.js
@@ -1,41 +1,45 @@
-var fs   = require('fs');
-var p    = require('path');
-var slug = require('slug');
-var url  = require('url');
-var util = require('util');
+import fs from 'fs';
+import p from 'path';
+import slug from 'slug';
+import url from 'url';
+import util from 'util';
 
 slug.defaults.mode ='rfc3986';
 
-var env = process.env['FEEDPAPER_ENV'];
-var conf_dir = process.env['FEEDPAPER_CFG'];
-var feedsCategories = JSON.parse(fs.readFileSync(p.join(conf_dir, env, 'feeds.json')));
-var conf = JSON.parse(fs.readFileSync(p.join(conf_dir, env, 'feedpaper.json')));
+const env = process.env['FEEDPAPER_ENV'];
+const conf_dir = process.env['FEEDPAPER_CFG'];
+const feedsCategories = JSON.parse(fs.readFileSync(p.join(conf_dir, env, 'feeds.json')));
+const conf = JSON.parse(fs.readFileSync(p.join(conf_dir, env, 'feedpaper.json')));
 
 // in lieu of AE86 pre-hook
-var apiBase = url.format({
+const apiBase = url.format({
   protocol: conf.api.protocol,
   hostname: conf.api.host,
   pathname: util.format('%d/%s', conf.api.version, conf.api.path)
 });
-var globalJs = fs.readFileSync(p.join('static', 'scripts', 'global.js.tpl'), 'utf-8');
+let globalJs = fs.readFileSync(p.join('static', 'scripts', 'global.js.tpl'), 'utf-8');
 globalJs = globalJs.replace(/var apiBase = 'PLACEHOLDER';/, util.format('var apiBase = \'%s\';', apiBase));
 fs.writeFileSync(p.join('static', 'scripts', 'global.js'), globalJs)
 
 function randomColour() {
   // App.js' colour scheme
-  COLOURS = ['teal', 'green', 'yellow', 'orange', 'red', 'dark-blue', 'blue'];
+  const COLOURS = ['teal', 'green', 'yellow', 'orange', 'red', 'dark-blue', 'blue'];
   return COLOURS[Math.round(Math.random() * 1000) % COLOURS.length];
 }
 
-exports.params = {
-  slug: function(title, cb) {
+const params = {
+  slug: function (title, cb) {
     cb(slug(title));
   },
-  app_topbar_colour: function(cb) {
+  app_topbar_colour: function (cb) {
     cb(randomColour());
   },
   sitemap: {
     'index.html': { title: 'Feedpaper' }
   },
   categories: feedsCategories
+};
+
+export {
+  params as params
 };
